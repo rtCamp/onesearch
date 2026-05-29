0eec653e777434e54c3b79588487d1e26b195d75
Kallyan Singha <singhakallyan@gmail.com>
2026-05-29 20:07:09 +0530

    feat: enhanced job management and UI for reindexing process

---
+++ phpstan/stubs/action-scheduler.php
@@ -0,0 +1,59 @@
+<?php // phpcs:disable -- This is a stub file for PHPStan analysis.
+/**
+ * Action Scheduler function and class stubs.
+ */
+
+namespace {
+	class ActionScheduler_Store {
+		public const STATUS_PENDING = 'pending';
+		public const STATUS_RUNNING = 'running';
+	}
+
+	class ActionScheduler_Action {
+		/**
+		 * @return array<string, mixed>
+		 */
+		public function get_args(): array {}
+	}
+
+	/**
+	 * @param string $hook
+	 * @param array<string, mixed> $args
+	 * @param string $group
+	 * @return int
+	 */
+	function as_enqueue_async_action( string $hook, array $args = [], string $group = '' ): int {}
+
+	/**
+	 * @param int $timestamp
+	 * @param string $hook
+	 * @param array<string, mixed> $args
+	 * @param string $group
+	 * @return int
+	 */
+	function as_schedule_single_action( int $timestamp, string $hook, array $args = [], string $group = '' ): int {}
+
+	/**
+	 * @param int $timestamp
+	 * @param int $interval_in_seconds
+	 * @param string $hook
+	 * @param array<string, mixed> $args
+	 * @param string $group
+	 * @return int
+	 */
+	function as_schedule_recurring_action( int $timestamp, int $interval_in_seconds, string $hook, array $args = [], string $group = '' ): int {}
+
+	/**
+	 * @param string $hook
+	 * @param array<string, mixed> $args
+	 * @param string $group
+	 * @return int|null
+	 */
+	function as_unschedule_action( string $hook, array $args = [], string $group = '' ): ?int {}
+
+	/**
+	 * @param array<string, mixed> $args
+	 * @return ActionScheduler_Action[]
+	 */
+	function as_get_scheduled_actions( array $args = [] ): array {}
+}
